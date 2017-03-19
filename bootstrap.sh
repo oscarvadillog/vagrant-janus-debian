fcdd0eae4f7b579a399d7dcca3f531d0dd8a0bd4
oscarvadillog <oscar.vadillog@gmail.com>
2017-03-19 14:11:30 +0100

    Updated --prefix value: /opt/janus

--- bootstrap.sh
+++ bootstrap.sh
@@ -45,7 +45,7 @@ export LD_LIBRARY_PATH
 cd / && git clone https://github.com/meetecho/janus-gateway.git
 cd janus-gateway
 sh autogen.sh
-./configure --prefix=/ --enable-docs
+./configure --prefix=/opt/janus --enable-docs
 make
 make install
 make configs
